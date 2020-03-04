81f4ea2253b2b2be7100999879da8c01e8d6b450
Brent Mello <brent.mello@sgdigital.com>
2020-03-04 17:22:28 -0600

    Life Points and Henchmen counters for unit rows now labeled. Villain rows now have counter on left and icon on right.

---
+++ Assets/Scripts/ZoneInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneInfo : MonoBehaviour
+{
+    //[SerializeField]
+    //Button tokenButton0, tokenButton1, tokenButton2;
+    //[SerializeField]
+    //Button heroButton0, heroButton1, heroButton2;
+
+    //void TokenButtonClicked()
+    //{
+    //    Debug.Log("TokenButtonClicked!!!");
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
