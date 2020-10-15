79ef83dfd84d36a638dd3c2f30fe2051171914cd
Brent Mello <bwmello@alaska.edu>
2020-10-15 10:31:55 -0500

    Fixed line of sight for zone 12 and added a test that calls GetLineOfSightWithZone for each zone on the Subway map.

---
+++ Assets/Tests/ZoneTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
+
+namespace Tests
+{
+    public class ZoneTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ZoneTestSimplePasses()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Subway.unity");  //Open the Scene in the Editor (do not enter Play Mode)
+            GameObject[] allZones = GameObject.FindGameObjectsWithTag("ZoneInfoPanel");
+            foreach (GameObject zone in allZones)
+            {
+                ZoneInfo currentZone = zone.GetComponent<ZoneInfo>();
+                foreach (GameObject losZone in currentZone.lineOfSightZones)
+                {
+                    ZoneInfo.LineOfSight lineOfSight = currentZone.GetLineOfSightWithZone(losZone);
+                    Assert.IsNotNull(lineOfSight);
+                }
+            }
+        }
+    }
+}
