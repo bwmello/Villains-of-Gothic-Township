f55969e4b0db4e653a3b030761512604552ae541
Brent Mello <bwmello@alaska.edu>
2021-05-14 11:14:45 -0500

    Modified background, mission rows, and spotlights of StartMenu scene to better accommodate all six mission rows

--- Assets/Scripts/MissionMenu.cs
+++ Assets/Scripts/MissionMenu.cs
@@ -15,21 +15,18 @@ public class MissionMenu : MonoBehaviour
     // Start is called before the first frame update
     private IEnumerator Start()
     {
-        //yield return new WaitForSecondsRealtime(Random.Range(0.5f, 1.0f));
         GameObject spotlight1 = Instantiate(spotlightPrefab, transform);
-        //Debug.Log("!!transform.width: " + transform.GetComponent<RectTransform>().rect.width.ToString() + "    transform.height: " + transform.GetComponent<RectTransform>().rect.height.ToString());
-        //Debug.Log("!!parent.transform.width: " + transform.parent.GetComponent<RectTransform>().rect.width.ToString() + "    parent.transform.height: " + transform.parent.GetComponent<RectTransform>().rect.height.ToString());
-        // parent_width=1920, x_center=-5, percentage_of_parent_width=-5/1920=-0.00260416666; parent_height=1080, y_center=170, percentage_of_parent_height=170/1080=0.1574074074
-        spotlight1.transform.localPosition = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * -0.00260416666), (float)(transform.GetComponent<RectTransform>().rect.height * 0.1574074074), 0);
-        // parent_width=1920, x_center=-140, percentage_of_parent_width=-140/1920=-0.07291666666; parent_height=1080, y_center=185, percentage_of_parent_height=185/1080=0.17129629629
-        spotlight1.GetComponent<Spotlight>().leftPoint = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * -0.07291666666), (float)(transform.GetComponent<RectTransform>().rect.height * 0.17129629629), 0);
+        // parent_width=1920, x_center=-5, percentage_of_parent_width=-5/1920=-0.00260416666; parent_height=1080, y_center=245, percentage_of_parent_height=245/1080=0.22685185185
+        spotlight1.transform.localPosition = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * -0.00260416666), (float)(transform.GetComponent<RectTransform>().rect.height * 0.22685185185), 0);
+        // parent_width=1920, x_center=-140, percentage_of_parent_width=-140/1920=-0.07291666666; parent_height=1080, y_center=265, percentage_of_parent_height=265/1080=0.24537037037
+        spotlight1.GetComponent<Spotlight>().leftPoint = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * -0.07291666666), (float)(transform.GetComponent<RectTransform>().rect.height * 0.24537037037), 0);
         yield return new WaitForSecondsRealtime(Random.Range(2.0f, 3.0f));
 
         GameObject spotlight2 = Instantiate(spotlightPrefab, transform);
-        // parent_width=1920, x_center=480, percentage_of_parent_width=-480/1920=0.25; parent_height=1080, y_center=215, percentage_of_parent_height=215/1080=0.19907407407
-        spotlight2.transform.localPosition = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * 0.25), (float)(transform.GetComponent<RectTransform>().rect.height * 0.19907407407), 0);
-        // parent_width=1920, x_center=390, percentage_of_parent_width=-390/1920=0.203125; parent_height=1080, y_center=95, percentage_of_parent_height=8=95/1080=0.08796296296
-        spotlight2.GetComponent<Spotlight>().leftPoint = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * 0.203125), (float)(transform.GetComponent<RectTransform>().rect.height * 0.08796296296), 0);
+        // parent_width=1920, x_center=485, percentage_of_parent_width=485/1920=0.25260416666; parent_height=1080, y_center=315, percentage_of_parent_height=315/1080=0.29166666666
+        spotlight2.transform.localPosition = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * 0.25260416666), (float)(transform.GetComponent<RectTransform>().rect.height * 0.29166666666), 0);
+        // parent_width=1920, x_center=375, percentage_of_parent_width=375/1920=0.1953125; parent_height=1080, y_center=160, percentage_of_parent_height=160/1080=0.14814814814
+        spotlight2.GetComponent<Spotlight>().leftPoint = new Vector3((float)(transform.GetComponent<RectTransform>().rect.width * 0.1953125), (float)(transform.GetComponent<RectTransform>().rect.height * 0.14814814814), 0);
         yield return null;
     }
 }
